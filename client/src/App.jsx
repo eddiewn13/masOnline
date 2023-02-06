0214601937af97f19486e8cb3b04a0826cda33c1
Eddie Wiik Nilsson <eddie.wiiknilsson@learnet.se>
2023-02-06 12:13:17 +0100

    Imported socket.io and simple message app implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 import DeckClass from './DeckClass'
+const socket = io.connect("http://127.0.0.1:3001");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  const sendMessage = () => {
+        socket.emit("send_message", { message });
+  };
+
+  useEffect(() => {
+        socket.on("receive_message", (data) => {
+              setMessageReceived(data.message);
+        });
+  }, [socket]);
+
+
+
   let Deck = new DeckClass()
-  console.log(Deck.GiveCard())
   console.log(Deck)
+
   return (
-   <div>
-      <button type="submit" onClick={() => func()}>Button</button>
-    <br />
-<input type="text" className="test"  readOnly></input>
-   </div>
+    <div className="App">
+    <input
+          type="text"
+          placeholder="Message..."
+          onChange={(event) => {
+                setMessage(event.target.value);
+          }}
+    />
+    <button onClick={sendMessage} type="submit">
+          Send Message
+    </button>
+    <h1> Message:</h1>
+    {messageReceived}
+</div>
+
   )
 }
 
